4f91d157f9f64860e8e4bba8434c362a92827df6
Heather Kim <heatherkim0405@gmail.com>
2022-05-10 02:58:06 -0500

    trying to solve 2 and 3

--- 2/2.js
+++ 2/2.js
@@ -1,11 +1,17 @@
 /**
  * 
  * @param {*} x 
+ * 
+ * any type of parameters are allowed, parameter is called x
  */
+
 const f = (x = 0) => {
+  //the function f takes in the parameter x, and the default value of x is 0
   console.log('x:', x)
+  //console log the value of x
   setImmediate(process.exit, 0)
   queueMicrotask(() => f(x + 1))
+  //anonymous function that adds 1 to x is executed
 }
 
 console.log(f());
--- 3/3.js
+++ 3/3.js
@@ -4,16 +4,24 @@
  * @param {*} x 
  * @param {*} y 
  * @param {*} z 
+ * 
+ * any type of parameters 
+ * parameter names are x, y, and z
  */
 const f = (x, y, z) => {
+  //function f takes in the parameters x, y, and z
   let p
+  // declare the variable p
   x.addEventListener('click', () => {
     p = false
+    //x can be any DOM element. When x is clicked, anonymous function that sets p to false is executed
     queueMicrotask(() => p = true)
+    // the anonymous function that sets p to true is executed
   })
   x.addEventListener('click', () => {
     p ? y() : z()
   })
+  //When x is clicked, the anonymous function is executed. if p is true, function y is invoked. if p is false, function z is invoked
 }
 
 
